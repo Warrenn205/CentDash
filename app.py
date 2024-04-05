1dde398e6f74b9fe7909a766d572344c6238b7ca
Warren Spencer <you@example.com>
2024-04-04 20:35:50 -0500

    Added Updates

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS user (
                     firstName TEXT NOT NULL,
                     lastName TEXT NOT NULL,
-                    email TEXT PRIMARY KEY NOT NULL,
+                    email TEXT NOT NULL,
                     password TEXT NOT NULL
                 )''')
 
@@ -67,33 +67,18 @@ def login():
     if request.method == "POST":
         email = request.form["Email"]
         password = request.form["Password"]
+        with get_db() as db:
+            cursor = db.cursor()
+            cursor.execute('SELECT * FROM user WHERE email = ? AND password = ?', (email, password))
+            user = cursor.fetchone()
 
-        if not email or not password:
-            return render_template("signin.html", errorMsg="Please enter both email and password.")
-
-        connection = sql.connect(DATABASE_NAME)
-        cursor = connection.cursor()
-        cursor.execute('SELECT * FROM user WHERE email = ?', (email,))
-        result = cursor.fetchall()
-
-        if not result or not result[0]:
-            connection.close()
-            return render_template("signin.html", errorMsg="This username is incorrect")
-
-        cursor.execute('SELECT * FROM user WHERE email = ? AND password = ?', (email, password))
-        result = cursor.fetchall()
-
-        if not result or not result[0]:
-            connection.close()
-            return render_template("signin.html", errorMsg="This password is incorrect")
-
-        connection.commit()
-        connection.close()
-
-        session["user"] = email
-        return redirect(url_for('welcome'))
+            if user:
+                session["user"] = email
+                return redirect(url_for('welcome'))
+            else:
+                return render_template("signin.html", errorMsg="Invalid email or password.")
     else:
-        return render_template("signin.html", errorMsg="Please enter both email and password.")
+        return render_template("signin.html", errorMsg="")
 
 @app.route('/signup', methods=["POST", "GET"])
 def signup():
@@ -104,35 +89,36 @@ def signup():
         password = request.form["Password"]
         confirmPassword = request.form["ConfirmPassword"]
 
-        def error(errorMsg):
-            return render_template("signup.html", errorMsg=errorMsg)
+        with get_db() as db:
+            cursor = db.cursor()
+            cursor.execute('SELECT * FROM user WHERE email = ?', (email,))
+            existing_user = cursor.fetchone()
 
-        if len(email) < 5:
-            return error("You must use a valid email")
-        elif len(email) > 100:
-            return error("You must use a valid email")
-        elif not "@" in email or not "." in email:
-            return error("Invalid email")
-        elif password != confirmPassword:
-            return error("The passwords must be the same")
-
-        connection = sql.connect(DATABASE_NAME)
-        cursor = connection.cursor()
-        cursor.execute('''INSERT INTO user (firstName, lastName, email, password) 
-                          VALUES (?, ?, ?, ?)''', (firstName, lastName, email, password))
-        cursor.execute('''INSERT INTO budget (user_id, data) VALUES (?, ?)''', (email, "{}"))
-        cursor.execute('''INSERT INTO net_worth (user_id, data) VALUES (?, ?)''', (email, "{}"))
-        connection.commit()
-        connection.close()
-
-        return redirect(url_for('login'))
+            if existing_user:
+                cursor.execute('''UPDATE user SET firstName = ?, lastName = ?, password = ? WHERE email = ?''',
+                               (firstName, lastName, password, email))
+                db.commit()
+                return redirect(url_for('login'))
+            else:
+                cursor.execute('''INSERT INTO user (firstName, lastName, email, password) 
+                                  VALUES (?, ?, ?, ?)''', (firstName, lastName, email, password))
+                db.commit()
+                return redirect(url_for('login'))
     else:
         return render_template("signup.html")
 
 
+@app.route('/forgot')
+def forgot_password():
+    return render_template("forgotpassword.html")
+
 @app.route('/welcome')
 def welcome():
-    return render_template("centdashpage.html")
+    if "user" in session:
+        user_email = session["user"]
+        return render_template("centdashpage.html", user_email=user_email)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/dashboard')
 def dashboard():
@@ -141,28 +127,28 @@ def dashboard():
 @app.route('/budgets', methods=['GET', 'POST'])
 def budgets():
     if request.method == 'POST':
-        user_id = session.get('user')
+        user_email = session.get('user')
         data = request.json.get('data')
 
         if data is None:
             return jsonify({'error': 'Data not provided in request body'}), 400
 
         json_data = json.dumps(data)
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute('''UPDATE budget SET data = ? WHERE user_id = ?''', (json_data, user_id))
-        db.commit()
+        with get_db() as db:
+            cursor = db.cursor()
+            cursor.execute('''UPDATE budget SET data = ? WHERE user_id = ?''', (json_data, user_email))
+            db.commit()
         return jsonify({'message': 'Data from Budgets saved successfully!'}), 200
     elif request.method == 'GET':
-        user_id = session.get('user')
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute('''SELECT data FROM budget WHERE user_id = ?''', (user_id,))
-        budgets_data = cursor.fetchone()
-        if budgets_data:
-            budgets_data = budgets_data[0]
-        else:
-            budgets_data = "{}"
+        user_email = session.get('user')
+        with get_db() as db:
+            cursor = db.cursor()
+            cursor.execute('''SELECT data FROM budget WHERE user_id = ?''', (user_email,))
+            budgets_data = cursor.fetchone()
+            if budgets_data:
+                budgets_data = budgets_data[0]
+            else:
+                budgets_data = "{}"
         return render_template("budgets.html", budgets_data=budgets_data)
     else:
         return jsonify({'error': 'Method not allowed'}), 405
@@ -170,23 +156,23 @@ def budgets():
 @app.route('/networth', methods=['GET', 'POST'])
 def networth():
     if request.method == 'POST':
-        user_id = session.get('user')
+        user_email = session.get('user')
         data = request.json.get('data')
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute('''UPDATE net_worth SET data = ? WHERE user_id = ?''', (data, user_id))
-        db.commit()
+        with get_db() as db:
+            cursor = db.cursor()
+            cursor.execute('''UPDATE net_worth SET data = ? WHERE user_id = ?''', (data, user_email))
+            db.commit()
         return jsonify({'message': 'Net worth data saved successfully'}), 200
     else:
-        user_id = session.get('user')
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute('''SELECT data FROM net_worth WHERE user_id = ?''', (user_id,))
-        networth_data = cursor.fetchone()
-        if networth_data:
-            networth_data = networth_data[0]
-        else:
-            networth_data = "{}" 
+        user_email = session.get('user')
+        with get_db() as db:
+            cursor = db.cursor()
+            cursor.execute('''SELECT data FROM net_worth WHERE user_id = ?''', (user_email,))
+            networth_data = cursor.fetchone()
+            if networth_data:
+                networth_data = networth_data[0]
+            else:
+                networth_data = "{}" 
         return render_template("networth.html", networth_data=networth_data)
 
 
@@ -228,9 +214,26 @@ def settings():
 def help():
     return render_template("help.html")
 
+@app.route('/delete_account', methods=['GET', 'POST'])
+def delete_account():
+    if request.method == 'POST':
+        user_email = session.get('user')
+        if user_email:
+            with get_db() as db:
+                cursor = db.cursor()
+                cursor.execute('UPDATE user SET email = NULL WHERE email = ?', (user_email,))
+                db.commit()
+            session.clear()
+            return redirect(url_for('login'))
+        else:
+            return redirect(url_for('login'))
+    else:
+        return render_template("settings.html")
+    
 @app.route('/logout')
 def logout():
-    return render_template("signin.html")
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True)
